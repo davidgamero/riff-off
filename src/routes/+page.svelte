791f672c639adaa482e399a381c74f1b9b1e9441
David Gamero <david340804@gmail.com>
2023-02-28 09:31:01 -0500

    add screen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Settings from '../Settings.svelte';
+</script>
+
+<div class="container flex min-h-screen flex-col items-center justify-center py-2">
+	<h1 class="text-4xl font-bold">Welcome to Riff Off!</h1>
+
+	<Settings />
+	<p>Start a new game</p>
+</div>
