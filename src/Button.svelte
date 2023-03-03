8a2f03e9e081a96e71b98c46065697ad8d66dab9
David Gamero <david340804@gmail.com>
2023-03-03 19:06:44 +0000

    update buttons and add text to play screen for time/pause/settings

--- src/Button.svelte
+++ src/Button.svelte
@@ -3,6 +3,6 @@
     export let link: string;
 </script>
 
-<a href={link}>
-    <button>{text}</button>
+<a href={link} class="w-full rounded-md bg-green-500 my-2 py-6 text-center text-white text-xl">
+    <p class="">{text}</p>
 </a>
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -4,7 +4,18 @@
 	let currentWord: string;
 </script>
 
-<div class="h-full flex flex-col justify-between align-middle">
+<div class="h-full flex flex-col justify-between align-middle p-6">
 	<PlayButton flipped={true} />
+	<div class="flex justify-between">
+		<div>
+		 <h1> Pause</h1>
+		</div>
+		<div>
+		 <h1> Timer</h1>
+		</div>
+		<div>
+		 <h1> Settings</h1>
+		</div>
+	</div>
 	<PlayButton />
 </div>
