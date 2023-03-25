7a4e6a9ccab2b60ee2825ad7b923401c9bb5722f
David Gamero <david340804@gmail.com>
2023-03-24 22:32:11 -0400

    scores and icons

--- src/PlayButton.svelte
+++ src/PlayButton.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
 	export let flipped: boolean = false;
+	export let score: number = 0;
+	const start: string = 'Got a Song!';
+	function handleClick() {
+		console.log('clicked');
+		score++;
+	}
 </script>
 
 <div class="bg-red-500 p-10 rounded-md">
-	<h1 class:upsidedown={flipped} class="text-center text-white text-2xl">Got a Song!</h1>
+	<h1 on:click={handleClick} class:upsidedown={flipped} class="text-center text-white text-2xl">
+		{#if score > 0}
+			{score}
+		{:else}
+			{start}
+		{/if}
+	</h1>
 </div>
 
 <style>
