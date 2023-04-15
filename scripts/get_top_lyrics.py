8b91c0e9fc4ef0ce00df0b8dc6423a3e2594ac6c
David Gamero <david340804@gmail.com>
2023-04-15 16:20:51 -0400

    spotify

---
+++ scripts/get_top_lyrics.py
@@ -0,0 +1,81 @@
+import os
+import time
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+from lyricsgenius import Genius
+import pickle
+
+load_dotenv()
+scope = "user-library-read"
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
+genius = Genius(os.getenv("GENIUS_ACCESS_TOKEN"))
+
+all_time_pop_hits_id = "6Mf614QiAuop5ud9x5beBS"
+
+# results = sp.current_user_saved_tracks()
+all_time_hits_playlist = sp.playlist(all_time_pop_hits_id)
+
+# open a file, where you stored the pickled data
+
+
+def load_lyrics_cache():
+    lyrics_cache = {}
+    try:
+        file = open('lyrics_cache', 'rb')
+        lyrics_cache = pickle.load(file)
+        file.close()
+    except FileNotFoundError:
+        file = open('lyrics_cache', 'wb')
+        pickle.dump({}, file)
+        file.close()
+    return lyrics_cache
+
+
+def save_lyrics_cache():
+    file = open('lyrics_cache', 'wb')
+    pickle.dump(lyrics_cache, file)
+    file.close()
+
+
+lyrics_cache = load_lyrics_cache()
+lyrics_dict = {}
+
+
+def parse_tracks_to_cache(tracks):
+
+    for idx, item in enumerate(tracks['items']):
+        track = item['track']
+        track_id = track['id']
+        print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+        lyrics = ""
+        if track_id in lyrics_cache:
+            print("Found in cache")
+            lyrics = lyrics_cache[track_id]
+        else:
+            print("Not found in cache")
+            # run ten times to avoid rate limit
+            tries = 0
+            while tries < 5 and lyrics == "":
+                tries += 1
+                try:
+                    genius_song = genius.search_song(
+                        track['name'], track['artists'][0]['name'])
+                    lyrics = genius_song.lyrics
+                    lyrics_cache[track_id] = lyrics
+                    save_lyrics_cache()
+                    print("Saved to cache")
+                except:
+                    print("Rate limit exceeded")
+                print("Retrying in ", 2**tries, " seconds")
+                time.sleep(2**tries)
+
+
+tracks = all_time_hits_playlist['tracks']
+while tracks['next']:
+    parse_tracks_to_cache(tracks)
+    tracks = sp.next(tracks)
+
+print(len(lyrics_dict))
---
+++ scripts/get_top_words_from_cache.py
@@ -0,0 +1,55 @@
+import pickle
+
+
+def load_lyrics_cache():
+    lyrics_cache = {}
+    try:
+        file = open('lyrics_cache', 'rb')
+        lyrics_cache = pickle.load(file)
+    except FileNotFoundError:
+        file = open('lyrics_cache', 'wb')
+        pickle.dump({}, file)
+        file.close()
+    return lyrics_cache
+
+
+def remove_non_letters(lyrics):
+    return "".join([char for char in lyrics if char.isalpha() or char == " " or char == "\n"])
+
+
+words_allow_list_file = open("./scripts/10k-english-no-swears.txt", "r")
+allowed_words = set()
+for line in words_allow_list_file:
+    allowed_words.add(line.strip())
+
+word_counts = {}
+lyrics_cache = load_lyrics_cache()
+for track_id, lyrics in lyrics_cache.items():
+    lyrics = remove_non_letters(lyrics)
+
+    # add words to set
+    word_set = set()
+    for word in lyrics.split():
+        word = word.lower()
+        if word in word_set or word not in allowed_words:
+            continue
+        word_set.add(word)
+
+    # add words from set to word_counts
+    for word in word_set:
+        if word in word_counts:
+            word_counts[word] += 1
+        else:
+            word_counts[word] = 1
+
+min_word_length = 4
+max_word_length = 10
+filtered_word_counts = {}
+for word, count in word_counts.items():
+    if len(word) >= min_word_length and len(word) <= max_word_length:
+        filtered_word_counts[word] = count
+
+sorted_word_counts = sorted(
+    filtered_word_counts.items(), key=lambda item: item[1], reverse=True)
+for word, count in sorted_word_counts[:100]:
+    print(word, count)
