259501dbd2ce3f4d2aa707b29166757144d43bcc
David Gamero <david340804@gmail.com>
2023-04-21 10:50:06 -0400

    more sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,8 @@
 </script>
 
 <div class="flex h-full flex-col items-center justify-center py-2">
-	<h1 class="font-pacifico text-4xl font-bold dark:text-blue-500 mb-5">Welcome to Riff Off!</h1>
+	<h2 class="font-pacifico text-2xl font-bold dark:text-gray-300 mb-5">Welcome to</h2>
+	<h1 class="font-pacifico text-6xl font-bold dark:text-blue-500 mb-8">Riff Off!</h1>
 
 	<Settings
 		bind:selectedGoal={goal}
