ce333c656a3d632de08a78dc6562c59caae4f7b7
David Gamero <david340804@gmail.com>
2023-04-21 09:31:14 -0400

    safari changes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,8 @@
 	import '../app.css';
 </script>
 
-<div class="dark:bg-gray-900">
-	<div class="max-w-md mx-auto h-screen ">
+<div class="dark:bg-gray-900 h-screen">
+	<div class="max-w-md mx-auto">
 		<slot />
 	</div>
 </div>
